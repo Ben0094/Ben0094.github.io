38db2d93f341e559d4a74f1c9c0c1e8847cddbeb
Ben0094 <158560355+Ben0094@users.noreply.github.com>
2024-05-27 20:02:41 -0400

    Add Beautiful Things mashup

---
+++ beautifulthings.js
@@ -0,0 +1,8 @@
+track_list.push(
+    {
+        name: "Beautiful Things",
+        artist: "Benson Boone / Our Last Night",
+        image: "beautifulthings.png",
+        path: "Beautiful Things.comp.001.m4a"
+    }
+);
