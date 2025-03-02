1da3971c4377ac6f34c974acff56b47d9bf80575
Ben0094 <158560355+Ben0094@users.noreply.github.com>
2025-03-02 03:16:26 -0500

    Add files via upload

--- beautifulthings.js
+++ beautifulthings.js
@@ -3,6 +3,6 @@ track_list.push(
         name: "Beautiful Things",
         artist: "Benson Boone / Our Last Night",
         image: "beautifulthings.png",
-        path: "Beautiful Things.comp.001.m4a"
+        path: "beautifulthings.m4a"
     }
 );
