ff634b27892b2f87561a6da043f9d1613937ed5f
Ben0094 <158560355+Ben0094@users.noreply.github.com>
2025-05-28 06:14:05 -0400

    brightside.001

---
+++ brightside.js
@@ -0,0 +1,14 @@
+track_list.push(
+    {
+        name: "Brightside",
+        artist: "",
+        image: "",
+        path: "brightside.001.m4a"
+    },
+    {
+        name: "Brightside vocals",
+        artist: "",
+        image: "",
+        path: "brightside.001.voxiso.m4a"
+    }
+);
