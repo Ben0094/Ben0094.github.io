5b2d0458b9e75a0b355de01df47c33f6ff58a959
Ben0094 <158560355+Ben0094@users.noreply.github.com>
2025-10-01 04:24:10 -0400

    Add files via upload

---
+++ downstairs.js
@@ -0,0 +1,8 @@
+track_list.push(
+    {
+        name: "Downstairs",
+        artist: "Twenty One Pilots",
+        image: "downstairs.png",
+        path: "Downstairs.001.m4a"
+    }
+);
---
+++ tally.js
@@ -0,0 +1,8 @@
+track_list.push(
+    {
+        name: "Tally",
+        artist: "Twenty One Pilots",
+        image: "tally.png",
+        path: "Tally.002.m4a"
+    }
+);
