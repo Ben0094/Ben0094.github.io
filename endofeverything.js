dd4b140f7139ba31be6bdbe62bdecd5752232d22
Ben0094 <158560355+Ben0094@users.noreply.github.com>
2026-05-25 02:34:37 -0400

    Add files via upload

---
+++ endofeverything.js
@@ -0,0 +1,8 @@
+track_list.push(
+    {
+        name: "End of Everything",
+        artist: "The Classic Crime",
+        image: "endofeverything.png",
+        path: "endofeverything.003.m4a"
+    }
+);
---
+++ happy.js
@@ -0,0 +1,8 @@
+track_list.push(
+    {
+        name: "HAPPY",
+        artist: "NF",
+        image: "Hope.png",
+        path: "HAPPY.004.m4a"
+    }
+);
